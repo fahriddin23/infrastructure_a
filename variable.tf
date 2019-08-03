5970d3b9d823363d7c28eb9de9e87d49414fab8c
fahriddin23 <fahriddin24@gmail.com>
2019-08-03 12:35:20 -0500

    done

--- variable.tf
+++ variable.tf
@@ -10,5 +10,22 @@ variable "Created_by" {
 variable "Dept" {
   
 }
+variable "ami" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "key_name" {
+  
+}
+  
+variable "count" {
+  
+}
+
+
+
+
 
 
